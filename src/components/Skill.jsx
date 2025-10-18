f464b96ef56d25e0335e8f57175571ccc400f352
MuhammadHarmainAli <codebyharmain@email.com>
2025-10-18 23:48:59 +0500

    Improved animation and responsiveness on home page

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -7,15 +7,15 @@ export const Skill = ({ hasAnimated }) => {
                 <div className={`transition-all duration-1000 delay-200 ${hasAnimated.skills ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-10'}`}>
                     <h2 className="text-3xl font-bold mb-9 text-center">Skills & Tachnologies</h2>
 
-                    <div className="grid md:grid-cols-3 gap-8">
+                    <div className="grid md:grid-cols-2 text-center gap-8">
                         {skill.map((skillGroup, index) => (
-                            <div key={index} className="bg-white p-8 rounded-lg border border-gray-200">
+                            <div key={index} className="w-96 mx-auto bg-white p-8 rounded-lg border border-gray-200">
                                 <h3 className="text-lg font-semibold mb-6 text-center">
                                     {skillGroup.category}
                                 </h3>
                                 <div className="space-y-3">
                                     {skillGroup.items.map((skill, index) => (
-                                        <div key={index} className="flex items-center">
+                                        <div key={index} className="flex items-center ">
                                             <div className="w-2 h-2 bg-black rounded-full mr-3"></div>
                                             <span className="text-gray-700">{skill }</span>
                                         </div>
--- src/data/skill.js
+++ src/data/skill.js
@@ -4,10 +4,6 @@ export const skill = [
         items: ["React.js", "Next.js", "JavaScript (ES6+)", "Tailwind CSS", "HTML5", "CSS3"] 
     },
     { 
-        category: "Backend", 
-        items: ["Node.js", "Express.js", "Firebase", "MongoDB"] 
-    },
-    { 
         category: "Tools & Others", 
         items: ["Git & GitHub", "Figma", "VS Code", "Vite", "Framer Motion"] 
     },
